690fb7d2280e694c30a508df9e8b732fb0d6b587
dmihn <dmihnea2004@gmail.com>
2022-05-21 23:24:42 +0300

    finished implementation

--- src/com/github/mihnead12/currrencycovert/Main.java
+++ src/com/github/mihnead12/currrencycovert/Main.java
@@ -1,7 +1,33 @@
 package com.github.mihnead12.currrencycovert;
 
 public class Main {
-    public static void main(String[] args) {
+    public static boolean isDouble(String val) {
+        try {
+            Double.parseDouble(val);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
 
+    public static void main(String[] args) throws IOException {
+        if (args.length == 0) {
+            System.out.println("No api key");
+            System.exit(-1);
+        }
+
+        String curr1, curr2;
+        double amount;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the currency you want to convert from: ");
+        curr1 = reader.readLine();
+
+        System.out.println("Enter the currency you want to convert to: ");
+        curr2 = reader.readLine();
+
+        System.out.println("Enter the amount: ");
+        String query = reader.readLine();
+        amount = (isDouble(query) ? Double.parseDouble(query) : 1);
+        System.out.println(CurrencyConverter.getInstance(args[0]).convert(amount, curr1, curr2));
     }
 }
---
+++ src/com/github/mihnead12/currrencycovert/web/WebDataFetcher.java
@@ -0,0 +1,43 @@
+package com.github.mihnead12.currrencycovert.web;
+
+import com.github.mihnead12.currrencycovert.web.exceptions.FetchError;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Objects;
+
+public class WebDataFetcher {
+
+    public static JSONObject fetchJSONObject(String url) throws FetchError {
+        try {
+            return new JSONObject(Objects.requireNonNull(fetchData(url)));
+        } catch (Exception ex) {
+            throw new FetchError(url);
+        }
+    }
+
+    public static String fetchData(String url) {
+        try {
+            URLConnection connection = new URL(url).openConnection();
+            connection.addRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            StringBuilder response = new StringBuilder();
+
+            for (String inputLine = in.readLine(); inputLine != null; inputLine = in.readLine()) {
+                response.append(inputLine);
+            }
+
+            in.close();
+
+            return response.toString();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+}
